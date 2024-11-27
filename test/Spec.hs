2b00aac45ee6f93234a3cd2eef62ce0dc8e71215
Harleen <harleen.singh-kaur@epitech.eu>
2024-11-27 14:17:04 +0100

    test: add unit test for ParseSExpr, basic test for test if it work

--- test/Spec.hs
+++ test/Spec.hs
@@ -7,3 +7,23 @@ main = hspec $ do
     describe "Example test" $ do
         it "should pass" $ do
             True `shouldBe` True
+
+    describe "StructureSe" $ do
+        it "should show Atom correctly" $ do
+            show (Atom "x") `shouldBe` "x"
+
+        it "should show List correctly" $ do
+            show (List [Atom "x", Atom "y"]) `shouldBe` "(x y)"
+
+    describe "Parser Lisp SE" $ do
+        it "parses a single atom" $ do
+            parseSExpr "x" `shouldBe` Just (Atom "x")
+
+        it "parse a simple list" $ do
+            parseSExpr "(x y)" `shouldBe` Just (List [Atom "x", Atom "y"])
+
+        it "parses a nested list" $ do
+            parseSExpr "(x (y z))" `shouldBe` Just (List [Atom "x", List [Atom "y", Atom "z"]])
+
+        it "returns Nothing for invalid input" $ do
+            parseSExpr "(x y" `shouldBe` Nothing
