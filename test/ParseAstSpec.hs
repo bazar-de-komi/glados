22a2dcef56b9ba79caf923a9f67bfe7a7961d0a0
Harleen <harleen.singh-kaur@epitech.eu>
2024-12-07 14:18:11 +0100

    test(ParseAST): unit test for ParseAST function

--- test/ParseAstSpec.hs
+++ test/ParseAstSpec.hs
@@ -2,7 +2,7 @@ module ParseAstSpec (spec) where
 
 import Test.Hspec
 import Structure (SExpr(..), AST(..))
-import Parser.ParserSExpAST (isInt, isBool, noMaybeParseAST)
+import Parser.ParserSExpAST (isInt, isBool, noMaybeParseAST, parseAST)
 
 spec :: Spec
 spec = do
@@ -31,6 +31,7 @@ spec = do
     describe "Utility Functions" $ do
         it "identifies integers with isInt" $ do
             isInt "123" `shouldBe` True
+            isInt "567p9" `shouldBe` False
             isInt "42" `shouldBe` True
             isInt "abc" `shouldBe` False
 
@@ -41,16 +42,38 @@ spec = do
             isBool "true" `shouldBe` False
 
     describe "noMaybeParseAST" $ do
-        it "parse an int atom into SInt" $ do
+        it "parses an int atom into SInt" $ do
             noMaybeParseAST (Atom "42") `shouldBe` Just (SInt 42)
 
-        it "parse a boolean atom into SBool" $ do
+        it "parses a boolean atom into SBool" $ do
             noMaybeParseAST (Atom "#t") `shouldBe` Just (SBool True)
             noMaybeParseAST (Atom "#f") `shouldBe` Just (SBool False)
 
-        it "parse a symbol atom into SSymbol" $ do
+        it "parses a symbol atom into SSymbol" $ do
             noMaybeParseAST (Atom "x") `shouldBe` Just (SSymbol "x")
 
         it "parses a list into SList" $ do
             noMaybeParseAST (List [Atom "42", Atom "x", Atom "#t"]) `shouldBe`
                 Just (SList [SInt 42, SSymbol "x", SBool True])
+
+    describe "parseAST" $ do
+        it "parses Nothing into Nothing" $ do
+            parseAST Nothing `shouldBe` Nothing
+
+        it "parses a valid integer SExpr into SInt" $ do
+            parseAST (Just (Atom "42")) `shouldBe` Just (SInt 42)
+
+        it "parses a valid boolean SExpr into SBool" $ do
+            parseAST (Just (Atom "#t")) `shouldBe` Just (SBool True)
+            parseAST (Just (Atom "#f")) `shouldBe` Just (SBool False)
+
+        it "parses a valid symbol SExpr into SSymbol" $ do
+            parseAST (Just (Atom "x")) `shouldBe` Just (SSymbol "x")
+
+        it "parses a list SExpr into SList AST" $ do
+            parseAST (Just (List [Atom "42", Atom "x", Atom "#t"])) `shouldBe`
+                Just (SList [SInt 42, SSymbol "x", SBool True])
+
+        it "handles nested lists correctly" $ do
+            parseAST (Just (List [Atom "x", List [Atom "42", Atom "#f"]])) `shouldBe`
+                Just (SList [SSymbol "x", SList [SInt 42, SBool False]])
