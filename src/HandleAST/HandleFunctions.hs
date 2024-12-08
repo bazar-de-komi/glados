16889f4ad67283cca3410d3f8fbc32037ce9de50
Eric <eric1.xu@epitech.eu>
2024-12-08 14:15:02 +0100

    feat(HandleAST): handleFunctions is now able to bind the parameters to its values in every cases

--- src/HandleAST/HandleFunctions.hs
+++ src/HandleAST/HandleFunctions.hs
@@ -1,13 +1,14 @@
 {-# LANGUAGE LambdaCase #-}
 
-module HandleAST.HandleFunctions (bindParameters) where
+module HandleAST.HandleFunctions (bindParameters, handleFunctions) where
 
 import Structure (AST (..))
 
--- | 
+-- |
 -- ==== Parameters
--- `AST`: The parameters of the function
--- `AST`: The value of the parameters
+-- - `AST`: The parameters of the function
+-- - `AST`: The value of the parameters
+--
 -- ==== Returns
 -- `Maybe AST`: Return a list of the parameters bind
 bindParameters :: AST -> AST -> Maybe AST
@@ -18,3 +19,20 @@ bindParameters (SList (parameter : paramTail)) (SList (value : valueTail)) =
         _ -> Nothing
 bindParameters (SList []) (SList []) = Just (SList [])
 bindParameters _ _ = Nothing
+
+-- |
+-- ==== Parameters
+-- - `AST`: Must be a copy of the entire ast
+-- - `AST`: Must be the entire function
+-- - `AST`: Must be the value of the parameters
+--
+-- ==== Returns
+-- `Maybe AST`: Return the result of the function
+handleFunctions :: AST -> AST -> AST -> Maybe AST
+handleFunctions ast (SList [SSymbol "define", SSymbol _, SList [SSymbol "lambda", params, body]]) values =
+    bindParameters params values
+handleFunctions ast (SList [SSymbol "define", SList [SSymbol _, params], body]) values =
+    bindParameters params values
+handleFunctions ast (SList [SSymbol "lambda", params, body]) values =
+    bindParameters params values
+handleFunctions _ _ _ = Nothing
--- test/HandleFunctionsSpec.hs
+++ test/HandleFunctionsSpec.hs
@@ -2,10 +2,16 @@ module HandleFunctionsSpec (spec) where
 
 import Test.Hspec
 import Structure (AST(..))
-import HandleAST.HandleFunctions (bindParameters)
+import HandleAST.HandleFunctions (bindParameters, handleFunctions)
 
 spec :: Spec
 spec = do
+    describe "HandleFunctions" $ do
+        testParameterBind
+        testHandleFunctions
+
+testParameterBind :: Spec
+testParameterBind =
     describe "Parameter binding test" $ do
         it "Bind one parameter" $ do
             let param = SList [
@@ -80,3 +86,104 @@ spec = do
             let value2 = SList [SInt 42, SInt 84]
             bindParameters param1 value1 `shouldBe` Nothing
             bindParameters param2 value2 `shouldBe` Nothing
+
+testHandleFunctions :: Spec
+testHandleFunctions = do
+    describe "Handle function basic test" $ do
+        it "Simple function" $ do
+            let ast = SList []
+            let function = SList [
+                            SSymbol "define",
+                            SList [
+                                SSymbol "add",
+                                SList [
+                                    SSymbol "x",
+                                    SSymbol "y"
+                                ]
+                            ],
+                            SList [
+                                SSymbol "+",
+                                SSymbol "x",
+                                SSymbol "y"
+                            ]
+                        ]
+            let values = SList [
+                            SInt 42,
+                            SInt 84
+                        ]
+            let result = SList [
+                            SList [
+                                SSymbol "x",
+                                SInt 42
+                            ],
+                            SList [
+                                SSymbol "y",
+                                SInt 84
+                            ]
+                        ]
+            handleFunctions ast function values `shouldBe` Just result
+
+        it "Simple lambda function" $ do
+            let ast = SList []
+            let function = SList [
+                            SSymbol "lambda",
+                            SList [
+                                SSymbol "x",
+                                SSymbol "y"
+                            ],
+                            SList [
+                                SSymbol "+",
+                                SSymbol "x",
+                                SSymbol "y"
+                            ]
+                        ]
+            let values = SList [
+                            SInt 5,
+                            SInt 10
+                        ]
+            let result = SList [
+                            SList [
+                                SSymbol "x",
+                                SInt 5
+                            ],
+                            SList [
+                                SSymbol "y",
+                                SInt 10
+                            ]
+                        ]
+            handleFunctions ast function values `shouldBe` Just result
+
+        -- Test pour une fonction avec un lambda après le nom
+        it "Function with lambda inside define" $ do
+            let ast = SList []
+            let function = SList [
+                            SSymbol "define",
+                            SSymbol "add",
+                            SList [
+                                SSymbol "lambda",
+                                SList [
+                                    SSymbol "x",
+                                    SSymbol "y"
+                                ],
+                                SList [
+                                    SSymbol "+",
+                                    SSymbol "x",
+                                    SSymbol "y"
+                                ]
+                            ]
+                        ]
+            let values = SList [
+                            SInt 7,
+                            SInt 3
+                        ]
+            let result = SList [
+                            SList [
+                                SSymbol "x",
+                                SInt 7
+                            ],
+                            SList [
+                                SSymbol "y",
+                                SInt 3
+                            ]
+                        ]
+            handleFunctions ast function values `shouldBe` Just result
