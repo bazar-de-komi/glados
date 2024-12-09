7e02af0734a6c189babdf15c59b2725d82b27d55
edouardclm <edouard.bocquet@epitech.eu>
2024-12-09 17:03:05 +0100

    feat: add double quote space

--- src/Parser/ParserLispSExp.hs
+++ src/Parser/ParserLispSExp.hs
@@ -26,7 +26,7 @@ takefstlist (a:b) c i
     | a == c && c == ')' && i == 0 = [a]
     | a == c && c == ')' = (a : (takefstlist b c (i - 1)))
     | a == c && c == ' ' = []
-    | a == c && c == '"' = []
+    | a == c && c == '"' = "\""
     | a == '(' && c == ')' = (a : (takefstlist b c (i + 1)))
     | otherwise = (a : (takefstlist b c i))
 
@@ -51,7 +51,7 @@ splitWords :: String -> [String]
 splitWords "" = []
 splitWords (a:b)
     | '(' == a = (a : (takefstlist (b) ')' 0)) : (splitWords(removefstlist (b) ')' 0))
-    | '"' == a = (takefstlist (b) '"' 0) : (splitWords(removefstlist (b) '"' 0))
+    | '"' == a = (a : takefstlist (b) '"' 0) : (splitWords(removefstlist (b) '"' 0))
     | ' ' == a = splitWords b
     | otherwise = (a : (takefstlist (b) ' ' 0)) : (splitWords(removefstlist (b) ' ' 0))
 
@@ -70,6 +70,8 @@ parseSExpr input =
   case unwords (words input) of
     '(' : xs | last xs == ')' -> parseList (init xs)
     '(' : _ -> Nothing
+    '"' : xs | last xs == '"' -> Just $ Atom (init xs)
+    '"' : _ -> Nothing
     x -> Just $ Atom x
   where
     parseList xs = Just . List $ mapMaybe parseSExpr (splitWords xs)
