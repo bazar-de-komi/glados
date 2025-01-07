68a9bdf6a56759701de3c9d65b054f2ec813316a
Harleen <harleen.singh-kaur@epitech.eu>
2025-01-07 14:36:35 +0100

    test(basic test): check if execution with a file work correctly

--- functionalTest/IntegrationSpec.hs
+++ functionalTest/IntegrationSpec.hs
@@ -3,12 +3,34 @@ module IntegrationSpec (spec) where
 import Test.Hspec
 import System.Process (readCreateProcessWithExitCode, shell)
 import System.Exit (ExitCode(..))
+import qualified Data.Text as T
 
-runGLaDOS :: String -> IO (ExitCode, String, String)
-runGLaDOS input = readCreateProcessWithExitCode (shell $ "echo '" ++ input ++ "' | ./glados") ""
+executable :: FilePath
+executable = "./glados"
+
+runTest :: FilePath -> IO (ExitCode, String, String)
+runTest inputFile = do
+    input <- readFile inputFile
+    readCreateProcessWithExitCode (shell $ "echo '" ++ input ++ "' | " ++ executable) ""
 
 spec :: Spec
-spec = do
-    describe "Example tests" $ do
-      it "should pass" $ do
-        1 `shouldBe` 1
+spec = do 
+    describe "Functional Tests : Basic fonctionality, check normal behavior" $ do
+      
+      it "handles simple condition expressions (if1.scm)" $ do
+        let inputFile = "functionalTest/inputs/if1.scm"
+        let expectedFile = "functionalTest/expected/if1.out"
+
+        (_, output, _) <- runTest inputFile
+        expectedOutput <- readFile expectedFile
+
+        T.unpack (T.strip (T.pack output)) `shouldBe` T.unpack (T.strip (T.pack expectedOutput))
+
+      it "evaluates an addition expression (addition.scm)" $ do
+        let addTestFile = "functionalTest/inputs/addition.scm"
+        let expectedTestFile = "functionalTest/expected/addition.out"
+  
+        (_, output, _) <- runTest addTestFile
+        expectedTestFile <- readFile expectedTestFile
+
+        T.unpack (T.strip (T.pack output)) `shouldBe` T.unpack (T.strip (T.pack expectedTestFile))
