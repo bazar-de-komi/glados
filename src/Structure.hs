76f9182c1dc101dc7893b6c5a50102a8ae913f21
Harleen <harleen.singh-kaur@epitech.eu>
2025-01-14 11:31:05 +0100

    feat(bytecode): add data structure for bytecode

--- src/Structure.hs
+++ src/Structure.hs
@@ -92,3 +92,56 @@ data AST
   | SList [AST]
       -- ^ Represents a list of AST nodes.
   deriving (Eq, Show)
+
+-- Data
+data Value
+  = VInt Int
+  | VFloat Float
+  | VBool Bool
+  | VString String
+  | VChar Char
+  deriving (Eq)
+
+instance Show Value where
+  show (VInt int) = show int
+  show (VBool bool) = show bool
+  show (VFloat float) = show float
+  show (VString string) = show string
+  show (VChar char) = show char
+
+data BinaryOperator
+  = ADD
+  | SUBTRACT
+  | MULTIPLY
+  | DIVIDE
+  | MODULO
+  deriving (Show, Eq)
+
+data BinaryComparator
+  = COMPARE_GT
+  | COMPARE_LT
+  | COMPARE_EQ
+  | COMPARE_NE
+  | COMPARE_GE
+  | COMPARE_LE
+  deriving (Show, Eq)
+
+-- Instruction
+data Instruction
+  = STORE_CONST Value
+  | STORE_VAR String
+  | LOAD_VAR String
+  | OPERATOR BinaryOperator
+  | COMPARATOR BinaryComparator
+  | JUMP String
+  | JUMP_IF_FALSE String
+  | LABEL String
+  | LABEL_FUNC String
+  | LABEL_FUNC_END String
+  | CALL String
+  | RETURN
+  | HALT
+  deriving (Show, Eq)
+
+-- Etat global pour générer des labels uniques
+data LabelState = LabelState { loopCounter :: Int, ifCounter :: Int }
