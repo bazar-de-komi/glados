013ff3ff3742eb4a95cde21419b8ced0e575d980
Harleen <harleen.singh-kaur@epitech.eu>
2025-01-19 20:02:21 +0100

    test(Strucure): fix probleme with nothing

--- test/LibsSpec.hs
+++ test/LibsSpec.hs
@@ -1,9 +1,8 @@
 module LibsSpec (spec) where
-
+    
 import Test.Hspec
 import Lib (litostr, needParenthese, checkparenthese, checkNotEnd, checkAllString)
-import Structure (Value(..), LabelState(..), Instruction(..), VM(..))
-import qualified Data.Map as Map
+import Structure (Value(..), LabelState(..))
 
 spec:: Spec
 spec = do
@@ -93,63 +92,3 @@ spec = do
           let state = LabelState (-1) (-2)
           loopCounter state `shouldBe` (-1)
           ifCounter state `shouldBe` (-2)
-
-        it "creates a VM with an empty stack and variables" $ do
-          let vm = VM [] Map.empty 0 Nothing []
-          stack vm `shouldBe` []
-          variables vm `shouldBe` Map.empty
-          index vm `shouldBe` 0
-          indexBeforeFuncCall vm `shouldBe` Nothing
-          instructions vm `shouldBe` []
-
-        it "allows adding values to the stack" $ do
-          let vm = VM [VInt 1, VBool True] Map.empty 0 Nothing []
-          stack vm `shouldBe` [VInt 1, VBool True]
-
-        it "allows storing variables" $ do
-          let vars = Map.fromList [("x", VInt 10), ("y", VFloat 3.14)]
-          let vm = VM [] vars 0 Nothing []
-          variables vm `shouldBe` vars
-
-        it "allows setting the instruction index" $ do
-          let vm = VM [] Map.empty 5 Nothing []
-          index vm `shouldBe` 5
-
-        it "allows setting the index before function call" $ do
-          let vm = VM [] Map.empty 0 (Just 3) []
-          indexBeforeFuncCall vm `shouldBe` Just 3
-
-        it "allows loading instructions" $ do
-          let instrs = [STORE_CONST (VInt 1), STORE_VAR "x"]
-          let vm = VM [] Map.empty 0 Nothing instrs
-          instructions vm `shouldBe` instrs
-
-        it "creates a fully populated VM" $ do
-            let launch = [VInt 1, VBool False]
-            let vars = Map.fromList [("x", VInt 10)]
-            let instrs = [STORE_CONST (VInt 1), STORE_VAR "x"]
-            let vm = VM launch vars 2 (Just 1) instrs
-            stack vm `shouldBe` launch
-            variables vm `shouldBe` vars
-            index vm `shouldBe` 2
-            indexBeforeFuncCall vm `shouldBe` Just 1
-            instructions vm `shouldBe` instrs
-
-        it "displays the first value of a non-empty stack" $ do
-          let vm = VM [VInt 42, VBool True] Map.empty 0 Nothing []
-          show vm `shouldBe` "42"          
-        it "displays a float value correctly" $ do
-          let vm = VM [VFloat 3.14, VInt 1] Map.empty 0 Nothing []
-          show vm `shouldBe` "3.14"        
-        it "displays a boolean value correctly" $ do
-          let vm = VM [VBool False, VInt 1] Map.empty 0 Nothing []
-          show vm `shouldBe` "False"           
-        it "displays a string value correctly" $ do
-          let vm = VM [VString "Hello", VInt 1] Map.empty 0 Nothing []
-          show vm `shouldBe` "\"Hello\""           
-        it "displays a char value correctly" $ do
-          let vm = VM [VChar 'A', VInt 1] Map.empty 0 Nothing []
-          show vm `shouldBe` "'A'"         
-        it "displays 'No return value' for an empty stack" $ do
-          let vm = VM [] Map.empty 0 Nothing []
-          show vm `shouldBe` "No return value"
