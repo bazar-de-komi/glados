a0d0f5fb0de4754f4d22ea35221ca1365a165cd2
Eric <eric1.xu@epitech.eu>
2025-01-19 19:23:28 +0100

    test(vm): add unit-test to the vm

--- test/RunVMSpec.hs
+++ test/RunVMSpec.hs
@@ -25,7 +25,7 @@ testInitializeVM = do
           stack = [],
           variables = Map.empty,
           index = 0,
-          indexBeforeFuncCall = Nothing,
+          callStack = [],
           instructions = generatedInstructions
       }
     it "initializes a VM with an empty stack, variables and instructions" $ do
@@ -34,63 +34,163 @@ testInitializeVM = do
           stack = [],
           variables = Map.empty,
           index = 0,
-          indexBeforeFuncCall = Nothing,
+          callStack = [],
           instructions = generatedInstructions
       }
 
 testFindLabel :: Spec
 testFindLabel = do
   describe "findLabel" $ do
-    it "finds the correct index for a label" $ do
-      let generatedInstructions = [LABEL "start", STORE_CONST (VInt 42), HALT]
-      findLabel "start" generatedInstructions `shouldBe` Just 0
-
     it "finds the correct index for a function end label" $ do
       let generatedInstructions = [LABEL_FUNC "func", LABEL_FUNC_END "func", STORE_CONST (VInt 42), HALT]
-      findLabel "func" generatedInstructions `shouldBe` Just 1
+      findLabel (LABEL_FUNC "func") generatedInstructions `shouldBe` Just 1
 
-    it "returns Nothing for a non-existent label" $ do
+    it "finds the correct index for a CALL instruction" $ do
+      let generatedInstructions = [CALL "func", LABEL_FUNC "func", LABEL_FUNC_END "func", STORE_CONST (VInt 42), HALT]
+      findLabel (CALL "func") generatedInstructions `shouldBe` Just 1
+
+    it "finds the correct index for a JUMP instruction" $ do
+      let generatedInstructions = [JUMP "label", LABEL "label", STORE_CONST (VInt 42), HALT]
+      findLabel (JUMP "label") generatedInstructions `shouldBe` Just 1
+
+    it "finds the correct index for a JUMP_IF_FALSE instruction" $ do
+      let generatedInstructions = [JUMP_IF_FALSE "label", LABEL "label", STORE_CONST (VInt 42), HALT]
+      findLabel (JUMP_IF_FALSE "label") generatedInstructions `shouldBe` Just 1
+
+    it "returns Nothing for a non-existent function end label" $ do
+      let generatedInstructions = [LABEL_FUNC "func", STORE_CONST (VInt 42), HALT]
+      findLabel (LABEL_FUNC "func") generatedInstructions `shouldBe` Nothing
+
+    it "returns Nothing for a non-existent CALL instruction" $ do
       let generatedInstructions = [STORE_CONST (VInt 42), HALT]
-      findLabel "missing" generatedInstructions `shouldBe` Nothing
+      findLabel (CALL "missing") generatedInstructions `shouldBe` Nothing
+
+    it "returns Nothing for a non-existent JUMP instruction" $ do
+      let generatedInstructions = [STORE_CONST (VInt 42), HALT]
+      findLabel (JUMP "missing") generatedInstructions `shouldBe` Nothing
+
+    it "returns Nothing for a non-existent JUMP_IF_FALSE instruction" $ do
+      let generatedInstructions = [STORE_CONST (VInt 42), HALT]
+      findLabel (JUMP_IF_FALSE "missing") generatedInstructions `shouldBe` Nothing
+
+    it "returns Nothing for a other instruction" $ do
+      let generatedInstructions = [STORE_CONST (VInt 42), HALT]
+      findLabel (STORE_VAR "missing") generatedInstructions `shouldBe` Nothing
 
 testExecute :: Spec
 testExecute = do
   describe "execute" $ do
-    it "executes STORE_CONST to push a value onto the stack" $ do
-      let vm = initializeVM []
-      let vm' = execute (STORE_CONST (VInt 42)) vm
-      stack vm' `shouldBe` [VInt 42]
-      index vm' `shouldBe` 1
+    describe "STORE_CONST" $ do
+      it "pushes a value onto the stack" $ do
+        let vm = initializeVM []
+        let vm' = execute (STORE_CONST (VInt 42)) vm
+        stack vm' `shouldBe` [VInt 42]
+        index vm' `shouldBe` 1
 
-    it "executes STORE_VAR to store a variable" $ do
-      let vm = (initializeVM []) { stack = [VInt 42] }
-      let vm' = execute (STORE_VAR "x") vm
-      variables vm' `shouldBe` Map.fromList [("x", VInt 42)]
-      stack vm' `shouldBe` []
-      index vm' `shouldBe` 1
+    describe "STORE_VAR" $ do
+      it "stores a variable from the stack" $ do
+        let vm = (initializeVM []) { stack = [VInt 42] }
+        let vm' = execute (STORE_VAR "x") vm
+        variables vm' `shouldBe` Map.fromList [("x", VInt 42)]
+        stack vm' `shouldBe` []
+        index vm' `shouldBe` 1
 
-    it "executes LOAD_VAR to load a variable onto the stack" $ do
-      let vm = (initializeVM []) { variables = Map.fromList [("x", VInt 42)] }
-      let vm' = execute (LOAD_VAR "x") vm
-      stack vm' `shouldBe` [VInt 42]
-      index vm' `shouldBe` 1
+      it "throws an error on empty stack" $ do
+        let vm = initializeVM []
+        evaluate (execute (STORE_VAR "x") vm) `shouldThrow` anyErrorCall
 
-    it "executes OPERATOR ADD for integers" $ do
-      let vm = (initializeVM []) { stack = [VInt 1, VInt 2] }
-      let vm' = execute (OPERATOR ADD) vm
-      stack vm' `shouldBe` [VInt 3]
-      index vm' `shouldBe` 1
+    describe "LOAD_VAR" $ do
+      it "loads a variable onto the stack" $ do
+        let vm = (initializeVM []) { variables = Map.fromList [("x", VInt 42)] }
+        let vm' = execute (LOAD_VAR "x") vm
+        stack vm' `shouldBe` [VInt 42]
+        index vm' `shouldBe` 1
 
-    it "executes COMPARATOR COMPARE_EQ for integers" $ do
-      let vm = (initializeVM []) { stack = [VInt 42, VInt 42] }
-      let vm' = execute (COMPARATOR COMPARE_EQ) vm
-      stack vm' `shouldBe` [VBool True]
-      index vm' `shouldBe` 1
+      it "throws an error for undefined variables" $ do
+        let vm = initializeVM []
+        evaluate (execute (LOAD_VAR "x") vm) `shouldThrow` anyErrorCall
 
-    it "executes HALT to stop the VM" $ do
-      let vm = initializeVM [HALT]
-      let vm' = execute HALT vm
-      index vm' `shouldBe` -1
+    describe "OPERATOR" $ do
+      it "executes ADD for integers" $ do
+        let vm = (initializeVM []) { stack = [VInt 1, VInt 2] }
+        let vm' = execute (OPERATOR ADD) vm
+        stack vm' `shouldBe` [VInt 3]
+        index vm' `shouldBe` 1
+
+      it "executes ADD for floats" $ do
+        let vm = (initializeVM []) { stack = [VFloat 1.5, VFloat 2.5] }
+        let vm' = execute (OPERATOR ADD) vm
+        stack vm' `shouldBe` [VFloat 4.0]
+        index vm' `shouldBe` 1
+
+      it "throws an error for stack underflow" $ do
+        let vm = initializeVM []
+        evaluate (execute (OPERATOR ADD) vm) `shouldThrow` anyErrorCall
+
+    describe "COMPARATOR" $ do
+      it "executes COMPARE_EQ for integers" $ do
+        let vm = (initializeVM []) { stack = [VInt 42, VInt 42] }
+        let vm' = execute (COMPARATOR COMPARE_EQ) vm
+        stack vm' `shouldBe` [VBool True]
+        index vm' `shouldBe` 1
+
+      it "executes COMPARE_GT for floats" $ do
+        let vm = (initializeVM []) { stack = [VFloat 1.5, VFloat 2.5] }
+        let vm' = execute (COMPARATOR COMPARE_GT) vm
+        stack vm' `shouldBe` [VBool True]
+        index vm' `shouldBe` 1
+
+      it "throws an error for stack underflow" $ do
+        let vm = initializeVM []
+        evaluate (execute (COMPARATOR COMPARE_EQ) vm) `shouldThrow` anyErrorCall
+
+    describe "JUMP" $ do
+      it "jumps to a label" $ do
+        let generatedInstructions = [LABEL "start", STORE_CONST (VInt 42), JUMP "start"]
+        let vm = initializeVM generatedInstructions
+        let vm' = execute (JUMP "start") vm
+        index vm' `shouldBe` 0
+
+      it "throws an error if the label is not found" $ do
+        let vm = initializeVM []
+        evaluate (execute (JUMP "unknown") vm) `shouldThrow` anyErrorCall
+
+    describe "JUMP_IF_FALSE" $ do
+      it "jumps if the top of the stack is False" $ do
+        let generatedInstructions = [LABEL "skip"]
+        let vm = (initializeVM generatedInstructions) { stack = [VBool False] }
+        let vm' = execute (JUMP_IF_FALSE "skip") vm
+        index vm' `shouldBe` 0
+        stack vm' `shouldBe` []
+
+      it "does not jump if the top of the stack is True" $ do
+        let generatedInstructions = [LABEL "skip"]
+        let vm = (initializeVM generatedInstructions) { stack = [VBool True] }
+        let vm' = execute (JUMP_IF_FALSE "skip") vm
+        index vm' `shouldBe` 1
+        stack vm' `shouldBe` []
+
+      it "throws an error for stack underflow" $ do
+        let vm = initializeVM []
+        evaluate (execute (JUMP_IF_FALSE "label") vm) `shouldThrow` anyErrorCall
+
+    describe "CALL" $ do
+      it "calls a function by jumping to its label" $ do
+        let generatedInstructions = [LABEL "func", RETURN, LABEL_FUNC "func"]
+        let vm = initializeVM generatedInstructions
+        let vm' = execute (CALL "func") vm
+        index vm' `shouldBe` 2
+        callStack vm' `shouldBe` [1]
+
+      it "throws an error if the function label is not found" $ do
+        let vm = initializeVM []
+        evaluate (execute (CALL "unknown") vm) `shouldThrow` anyErrorCall
+
+    describe "HALT" $ do
+      it "stops the VM" $ do
+        let vm = initializeVM [HALT]
+        let vm' = execute HALT vm
+        index vm' `shouldBe` -1
 
 testExecuteInstructions :: Spec
 testExecuteInstructions = do
@@ -111,3 +211,20 @@ testRunVM = do
       let vm = runVM generatedInstructions
       stack vm `shouldBe` [VInt 42]
       index vm `shouldBe` -1
+
+    it "runs a VM with complex function call bytecode" $ do
+      let generatedInstructions =
+              [ LABEL_FUNC "main"
+              , STORE_CONST (VInt 10)
+              , CALL "addOne"
+              , RETURN
+              , LABEL_FUNC_END "main"
+              , LABEL_FUNC "addOne"
+              , STORE_CONST (VInt 1)
+              , OPERATOR ADD
+              , RETURN
+              , LABEL_FUNC_END "addOne"
+              , CALL "main"
+              ]
+      let vm = runVM generatedInstructions
+      stack vm `shouldBe` [VInt 11]
